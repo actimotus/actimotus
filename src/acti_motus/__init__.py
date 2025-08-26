273870df9942badb59dcdd44253e1ed255e43946
Josef Heidler <heidlerjosef@gmail.com>
2025-08-26 09:03:11 +0200

    Added Sens bin/buffer parser

--- src/acti_motus/__init__.py
+++ src/acti_motus/__init__.py
@@ -2,10 +2,12 @@ from .activities import Activities
 from .classifications import References
 from .exposures import Exposures
 from .features import Features
+from .sens_parser import Sens
 
 __all__ = [
     'Exposures',
     'Features',
     'Activities',
     'References',
+    'Sens',
 ]
