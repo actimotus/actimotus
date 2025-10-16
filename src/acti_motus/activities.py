dcb0d9a638c2f13228cb06583747db1f967d29b6
JonRoslyng <xjla@nfa.dk>
2025-10-16 08:17:48 +0200

    Import legacy config to activities

--- src/acti_motus/activities.py
+++ src/acti_motus/activities.py
@@ -8,7 +8,14 @@ import pandas as pd
 
 from .classifications import Arm, Calf, References, Thigh, Trunk
 from .iterators import DataFrameIterator
-from .settings import ACTIVITIES, CONFIG, FEATURES, SENS__ACTIVITY_VALUES, SENS__FLOAT_FACTOR
+from .settings import (
+    ACTIVITIES,
+    CONFIG,
+    LEGACY_CONFIG,
+    FEATURES,
+    SENS__ACTIVITY_VALUES,
+    SENS__FLOAT_FACTOR,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -16,11 +23,11 @@ logger = logging.getLogger(__name__)
 @dataclass
 class Activities:
     system_frequency: int = 30
-    vendor: Literal['Sens', 'Other'] = 'Other'
+    vendor: Literal["Sens", "Other"] = "Other"
     orientation: bool = True
     chunks: bool = False
-    size: str | timedelta = '1d'
-    overlap: str | timedelta = '15min'
+    size: str | timedelta = "1d"
+    overlap: str | timedelta = "15min"
     config: dict[str, Any] | None = None
 
     def __post_init__(self):
@@ -42,7 +49,7 @@ class Activities:
         arm: pd.DataFrame | None = None,
         references: References,
     ) -> tuple[pd.DataFrame, References]:
-        not_overlaps = thigh[~thigh['overlap']]
+        not_overlaps = thigh[~thigh["overlap"]]
         start, end = not_overlaps.index[0], not_overlaps.index[-1]
 
         if trunk is not None:
@@ -104,7 +111,7 @@ class Activities:
         arm: pd.DataFrame | None = None,
     ) -> tuple[pd.DataFrame, References]:
         if thigh.empty:
-            raise ValueError('Thigh data is empty. Please provide valid thigh data.')
+            raise ValueError("Thigh data is empty. Please provide valid thigh data.")
 
         activities = Thigh(
             orientation=self.orientation,
@@ -112,21 +119,25 @@ class Activities:
             vendor=self.vendor,
             config=self.config,
         ).compute_activities(thigh, references=references)
-        logger.info('Detected activities for thigh.')
+        logger.info("Detected activities for thigh.")
 
         if isinstance(trunk, pd.DataFrame) and not trunk.empty:
-            activities = Trunk(orientation=self.orientation, config=self.config).compute_activities(
-                trunk, activities, references=references
-            )
-            logger.info('Detected activities for trunk.')
+            activities = Trunk(
+                orientation=self.orientation, config=self.config
+            ).compute_activities(trunk, activities, references=references)
+            logger.info("Detected activities for trunk.")
 
         if isinstance(calf, pd.DataFrame) and not calf.empty:
-            activities = Calf(orientation=self.orientation).compute_activities(calf, activities)
-            logger.info('Detected activities for calf.')
+            activities = Calf(orientation=self.orientation).compute_activities(
+                calf, activities
+            )
+            logger.info("Detected activities for calf.")
 
         if isinstance(arm, pd.DataFrame) and not arm.empty:
-            activities = activities.join(Arm(orientation=self.orientation).compute_activities(arm), how='left')
-            logger.info('Detected activities for arm.')
+            activities = activities.join(
+                Arm(orientation=self.orientation).compute_activities(arm), how="left"
+            )
+            logger.info("Detected activities for arm.")
 
         references.remove_outdated(activities.index[-1])
 
@@ -166,19 +177,21 @@ class Activities:
     def _map_activities(
         self,
         series: pd.Series,
-        input: Literal['text', 'numeric'] = 'text',
+        input: Literal["text", "numeric"] = "text",
     ) -> pd.Series:
         match input:
-            case 'text':
+            case "text":
                 code_to_activity = {v: k for k, v in ACTIVITIES.items()}
                 series = series.map(code_to_activity)
-            case 'numeric':
+            case "numeric":
                 series = series.map(ACTIVITIES)
             case _:
                 raise ValueError("Input must be either 'text' or 'numeric'.")
 
         if series.isna().all():
-            raise ValueError('No valid activities found in the series. Please check the input data.')
+            raise ValueError(
+                "No valid activities found in the series. Please check the input data."
+            )
 
         return series
 
@@ -194,24 +207,28 @@ class Activities:
         # "activity" is not part of SENS__ACTIVITY_VALUES, because it is always present in the dataframe
         for value in values:
             if value not in origin.columns:
-                df[value] = 0  # If column is missing, fill with 0. Usually because not all sensors are present
+                df[value] = (
+                    0  # If column is missing, fill with 0. Usually because not all sensors are present
+                )
             else:
                 df[value] = origin[value]
 
         df[values] = df[values].fillna(0)  # Fill NaN values with 0
-        df['activity'] = self._map_activities(origin['activity'], 'text').astype(np.int16)  # From the origin dataframe
+        df["activity"] = self._map_activities(origin["activity"], "text").astype(
+            np.int16
+        )  # From the origin dataframe
 
         df[values] = (df[values] * SENS__FLOAT_FACTOR).astype(np.int32)
-        df['verbose'] = 0
-        df['verbose'] = df['verbose'].astype(np.int32)
+        df["verbose"] = 0
+        df["verbose"] = df["verbose"].astype(np.int32)
 
         df.index = df.index.astype(np.int64) // 10**6  # Time in milliseconds
 
         return (
             df.index.values,
-            df['activity'].values,
+            df["activity"].values,
             df[values].values,
-            df['verbose'].values,
+            df["verbose"].values,
             references,
         )  # type: ignore
 
@@ -221,8 +238,8 @@ class Activities:
         data: np.ndarray,
     ) -> pd.DataFrame:
         df = pd.DataFrame(data, index=timestamps, columns=FEATURES)
-        df.index = pd.to_datetime(df.index, unit='ms')  # type: ignore
-        df.index.name = 'datetime'  # type: ignore
+        df.index = pd.to_datetime(df.index, unit="ms")  # type: ignore
+        df.index.name = "datetime"  # type: ignore
 
         df[FEATURES] = (df[FEATURES] / SENS__FLOAT_FACTOR).astype(np.float32)
 
@@ -232,7 +249,9 @@ class Activities:
         self,
         timestamps: np.ndarray,
         data: np.ndarray,
-    ) -> tuple[pd.DataFrame, pd.DataFrame | None, pd.DataFrame | None, pd.DataFrame | None]:
+    ) -> tuple[
+        pd.DataFrame, pd.DataFrame | None, pd.DataFrame | None, pd.DataFrame | None
+    ]:
         thigh, trunk, calf, arm = None, None, None, None
 
         thigh = self._parse_sensor_features(timestamps[0], data[0][:, 1:])
@@ -256,6 +275,8 @@ class Activities:
     ) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, dict[str, Any]]:
         thigh, trunk, calf, arm = self._raw_from_sens(timestamps, data)
 
-        df, references = self.compute(thigh=thigh, trunk=trunk, calf=calf, arm=arm, references=references)
+        df, references = self.compute(
+            thigh=thigh, trunk=trunk, calf=calf, arm=arm, references=references
+        )
 
         return self.to_sens(df, references)
